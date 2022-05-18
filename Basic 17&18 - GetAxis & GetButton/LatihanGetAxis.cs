b86d64bb8b59940a838246466f6709e1a31e1b18
Rarazu <rara_zahra_29rpl@student.smktelkom-mlg.sch.id>
2022-05-18 21:11:23 +0700

    Basic C# Unity

---
+++ Basic 17&18 - GetAxis & GetButton/LatihanGetAxis.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LatihanGetAxis : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal") * 10f * Time.deltaTime;
+        float v = Input.GetAxis("Vertical") * 10f * Time.deltaTime;
+
+        transform.Translate(h, 0.0f, v);
+    }
+}
---
+++ Basic 17&18 - GetAxis & GetButton/LatihanGetButton.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LatihanGetButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    float angka;
+    bool tambah;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            tambah = true;
+        }
+
+        if (Input.GetButtonUp("Fire1"))
+        {
+            tambah = false;
+        }
+
+        if (tambah)
+        {
+            angka += Time.deltaTime;
+        }
+
+        Debug.Log((int)angka);
+    }
+}
